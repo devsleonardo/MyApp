11e74183dd1d4183f0064a368d8407370274bfe2
cod3rleo <leonardo.cod3r@gmail.com>
2021-09-28 10:53:06 -0300

    edit commit 2.0

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -19,8 +19,6 @@ function Navbar(){
 
         { useSelector(state => state.usuarioLogado) > 0 ? 
         <>
-          <li className="nav-item"><Link to="" className="nav-link active text-white font-weight-bold" >Publicar envento</Link></li>        
-          <li className="nav-item"><Link to="" className="nav-link active text-white font-weight-bold" >Meus Eventos</Link></li>
           <li className="nav-item"><Link onClick={() => dispatch({type: 'LOG_OUT'})} className="nav-link active text-white font-weight-bold">Deslogar</Link></li>
         </> 
         :
